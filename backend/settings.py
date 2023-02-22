e312cc93144ee889d39470712d026f6debc80ce5
Ifihan <victoriaolusheye@gmail.com>
2023-02-22 18:32:23 +0100

    Chore: Made change to the deployment

--- backend/settings.py
+++ backend/settings.py
@@ -1,3 +1,5 @@
+import os
+
 from pathlib import Path
 from decouple import config
 
@@ -26,6 +28,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -97,3 +100,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
